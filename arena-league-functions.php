19f342fa5aa36b8c8b00e5e23aa7497018f6058e
Cooper <Mac.Cooper@uhsinc.com>
2023-08-28 16:05:53 -0400

    Test using GamefaceLeagueSetup as global data object for leagues

--- arena-league-functions.php
+++ arena-league-functions.php
@@ -15,18 +15,36 @@ class GamefaceLeagueSetup {
     $this->charset = $wpdb->get_charset_collate();
     $this->tablename = $wpdb->prefix . "gameface_leagues";
 
+    /* SETUP ENVIRONMENT */
     add_action('activate_gameface-beta/arena-league-functions.php', array($this, 'onActivate'));
-    
     add_action('init', array($this, 'setup_url'));
-
-    add_action('admin_post_createleague', array($this, 'createLeague'));
-    add_action('admin_post_nopriv_createleague', array($this, 'createLeague'));
-
-    add_action('admin_post_deleteleague', array($this, 'deleteLeague'));
-    add_action('admin_post_nopriv_deleteleague', array($this, 'deleteLeague'));
-
     add_action('wp_enqueue_scripts', array($this, 'loadAssets'));
     add_filter('template_include', array($this, 'loadTemplate'), 99);
+
+    // DATABASE ACTIONS SETUP
+    // === CREATE
+    add_action('admin_post_createleague', array($this, 'createLeague'));
+    add_action('admin_post_nopriv_createleague', array($this, 'createLeague'));
+    // === DELETE
+    add_action('admin_post_deleteleague', array($this, 'deleteLeague'));
+    add_action('admin_post_nopriv_deleteleague', array($this, 'deleteLeague'));
+    // === UPDATE
+    add_action('admin_post_updateleague', array($this, 'updateLeague'));
+    add_action('admin_post_nopriv_updateleague', array($this, 'updateLeague'));
+
+  }
+
+  function onActivate() {
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta("CREATE TABLE $this->tablename (
+      id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+      leagueName varchar(60) NOT NULL DEFAULT '',
+      leagueLink varchar(60) NOT NULL DEFAULT '',
+      numTeams smallint(5) NOT NULL DEFAULT 0,
+      teamSize smallint(5) NOT NULL DEFAULT 0,
+      game varchar(60) NOT NULL DEFAULT '',
+      PRIMARY KEY  (id)
+    ) $this->charset;");
   }
 
   function loadAssets() {
@@ -44,7 +62,6 @@ class GamefaceLeagueSetup {
 
   function setup_url() {
     // league-route.php catches these requests
-    
     // accept and access these url parameters
     global $wp; 
     $wp->add_query_var('l');
@@ -59,20 +76,19 @@ class GamefaceLeagueSetup {
 
   }
 
-  function onActivate() {
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta("CREATE TABLE $this->tablename (
-      id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-      leagueName varchar(60) NOT NULL DEFAULT '',
-      leagueLink varchar(60) NOT NULL DEFAULT '',
-      numTeams smallint(5) NOT NULL DEFAULT 0,
-      teamSize smallint(5) NOT NULL DEFAULT 0,
-      game varchar(60) NOT NULL DEFAULT '',
-      PRIMARY KEY  (id)
-    ) $this->charset;");
+  function deleteLeague() {
+    if(current_user_can('administrator')) {
+      $id = sanitize_text_field($_POST['idtodelete']);
+      global $wpdb;
+      $wpdb->delete($this->tablename, array('id' => $id));
+      wp_safe_redirect(site_url('/leagues'));
+    } else {
+      wp_safe_redirect(site_url());
+    }
+    exit;
   }
 
-  function deleteLeague() {
+  function updateLeague() {
     if(current_user_can('administrator')) {
       $id = sanitize_text_field($_POST['idtodelete']);
       global $wpdb;
@@ -99,6 +115,13 @@ class GamefaceLeagueSetup {
       wp_safe_redirect(site_url());
     }
     exit;
+  }
+
+  // ACCESS DB ACTIONS
+  function selectSingle($l) {
+    global $wpdb;
+    $query = "SELECT * FROM $this->$tablename WHERE leagueLink = '$l'";
+    return $wpdb->get_results($wpdb->prepare($query));
   }
 }
 
