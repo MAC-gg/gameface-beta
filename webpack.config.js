c4281f9fed2850466c56d1b069551a79e230c0d3
MAC-gg <mc669756@gmail.com>
2023-09-18 08:27:21 -0400

    npm almost setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        user_actions: "./js/user_actions.js"
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'bundled/js'),
-    },
     mode: 'development', // development = no minify, production = minify
+    entry: {
+        user_actions: "./js/user_actions.js",
+        user_styles: "./scss/user_styles.scss"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin ({filename:"css/[name].css"})
+    ],
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'bundled'),
+    }
 }
