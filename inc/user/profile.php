ac3c02d6d90edd5659024ddaafc0daf8e4c21ea6
MAC-gg <mc669756@gmail.com>
2023-10-22 13:31:08 -0400

    user profile default styles

--- inc/user/profile.php
+++ inc/user/profile.php
@@ -1,18 +1,23 @@
-<div class="container">
+<div class="container cw-profile">
     <div class="cw-banner-img"></div>
     <div class="cw-profile-header-bar">
+        <img src="<?php echo $cwGlobal->plugin_url; ?>img/profile-img-default.jpg" />
         <div class="cw-profile-header">
-            <img src="<?php echo $cwGlobal->plugin_url; ?>img/profile-img-default.jpg" />
             <div class="cw-profile-header-info">
                 <h1><?php echo $u; ?></h1>
                 <p>This is where a player's status might go...</p>
+                <div class="cw-profile-socials">
+                    <p title="Click to Copy"><i class="bi bi-discord"></i> <strong>Discord:</strong> someuser#1234</p>
+                </div>
             </div>
+            <?php if($owner) : ?>
+                <div class="cw-actions">
+                    <a class="btn btn-primary" href="/u/<?php echo $u; ?>/edit">Edit Profile</a>
+                    <a class="btn btn-secondary" href="/u/<?php echo $u; ?>/account">Edit Account</a>
+                </div>
+            <?php endif; ?>
         </div>
-        <?php if($owner) : ?>
-            <div class="cw-actions">
-                <a class="btn btn-primary" href="/u/<?php echo $u; ?>/edit">Edit Profile</a>
-                <a class="btn btn-secondary" href="/u/<?php echo $u; ?>/account">Edit Account</a>
-            </div>
-        <?php endif; ?>
+    </div>
+    <div style="background-color:#efefef;/*padding:2.5rem;*/">
     </div>
 </div>
